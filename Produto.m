e8255c590a120bf60266fee07442e396f21a32dc
jovi25 <41312313+jovi25@users.noreply.github.com>
2018-11-12 13:00:38 -0200

    Add files via upload

--- Produto.m
+++ Produto.m
@@ -1,15 +1,19 @@
 function [R,Rm] = Produto(Q,Qm,Qi,S,Sm,Si)
 
+% Encontrando os estados iniciais
+
 qnext(1) = find(Qi == 1);
 snext(1) = find(Si == 1);
 Reach(1,:) = [qnext(1) snext(1)];
 
+R{1}{1} = {[qnext(1) snext(1)]};
+
 % Produto
 
-wdone=0; iget=0; iset=1; l=1; qhelp=0;
-while wdone==0
+iget=0; iset=1; l=1; ir=0; saant=0; ireach=1;
+while 1
     i=0;
-    iget = iget+1; 
+    iget = iget+1;
     
     if(iget>length(qnext))
         break
@@ -34,66 +38,60 @@ while wdone==0
         end
     end
     
+    if(saant~=sa || qaant~=qa)
+        ir = ir+1;
+    end
     
     for k=1:numel(Q{qa})
         
-        if(~isempty(Q{qa}{k}))
-            auxq1 = Q{qa}{k}(1);
-            auxq2 = Q{qa}{k}(2);
-        else
-            k=k+1;
-        end
-
+        auxq1 = Q{qa}{k}(1);
+        auxq2 = Q{qa}{k}(2);
         
         for j=1:numel(S{sa})
             
-            if(~isempty(S{sa}{j}))
-                auxs1 = S{sa}{j}(1);
-                auxs2 = S{sa}{j}(2);
-            else
-            j=j+1;
-            end
+            auxs1 = S{sa}{j}(1);
+            auxs2 = S{sa}{j}(2);
             
             if(auxq2 == auxs2)
                 
                 i=i+1;
                 iset=iset+1;                
                 
+                
+                if(i>1)
+                    R{ir}{i-1}={[qa sa] [qhelp shelp] thelp};
+                end
+                    
+                R{ir}{i}={[qa sa] [auxq1 auxs1] auxs2};
+                
+                qhelp=auxq1;
+                shelp=auxs1;
+                thelp=auxq2;
+                
+                
+                % Verificando se o produto já passou pelo estado futuro
                 jump=0;
                 for m=1:length(Reach(:,1)) 
                     if (Reach(m,1)==auxq1 && Reach(m,2)==auxs1)
                         jump=1;
-                        qhelp=auxq1;
-                        shelp=auxs1;
-                        thelp=auxq2;
                     end
                 end
+   
                 
                 if(jump==0)
                     
-                    if(i>1 && qhelp~=0)
-                        R{iget}{i-1}={[qhelp shelp] thelp};
-                    end
-                    
-                    R{iget}{i}={[auxq1 auxs1] auxs2};
+                    ireach=ireach+1;
                     qnext(iset) = auxq1;
                     snext(iset) = auxs1;
-                    Reach(iget,:) = [auxq1 auxs1];
-                    
-                    l=l+1;
-                    if(Qm(auxq1)==1 && Sm(auxs1)==1)
-                        Rm(l)=1;
-                    else
-                        Rm(l)=0;
-                    end
-                    
+                    Reach(ireach,:) = [auxq1 auxs1];   
                     
                 end
                 
             end
         end
     end
-
+    saant = sa;
+    qaant = qa;
 end
 
 R = R(~cellfun('isempty',R));
